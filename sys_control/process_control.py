439ce42b9051a44016e105c959beaecb63fcdf3e
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2025-08-19 15:22:05 -1000

    Changed transform and inference to a functional style.

--- sys_control/process_control.py
+++ sys_control/process_control.py
@@ -6,16 +6,28 @@ Spec:   Handle all processes and their orderings.
 Usage:  python3 sys_control/process_control.py 
 '''
 
-# TODO: 
+import sys
+import os
 
-# Check to make sure the forced shutdown flag is false 
+# Add project root to sys.path so audio_transform can be imported
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
 
-# Set the forced shutdown flag true
+import sys_control.transform_and_inference as transform_and_inference
+import audio_transform.audio_to_spectro as audio_to_spectro
+import audio_transform.dat_to_wav as dat_to_wav
+import image_inference.inference as inference
 
-# Create a timer the shuts down the pi after X minutes 
 
-# call select audio 
+# TODO: Check to make sure the forced shutdown flag is false 
 
-# call transform and inference with output from select audio
+# TODO: Set the forced shutdown flag true
 
-# Set the forced shutdown flag false
+# TODO: Create a timer the shuts down the pi after X minutes 
+
+# TODO: call select audio 
+
+
+# TODO: call transform and inference with output from select audio
+
+# TODO: Set the forced shutdown flag false

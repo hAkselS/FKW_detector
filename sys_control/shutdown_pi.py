c563a2603291101d1e92592f0bb529faf87e455b
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2025-10-24 14:06:03 -1000

    Finished shutdown.py... after fiasco.

--- sys_control/shutdown_pi.py
+++ sys_control/shutdown_pi.py
@@ -7,13 +7,33 @@ Spec:   Handle the graceful shutdown of the Raspberry Pi system.
 ID:     sp
 '''
 
-def shutdown(reason): 
 
+from subprocess import call
+import time
+
+# TODO: move this to config file
+config_GPIO_on_time = 10
+config_GPIO_pin_num = 11 # Pin 11 should be a GPIO only pin
+
+def raise_GPIO(pin_number, wait_time):
+    if (wait_time < 10):
+        print(f"\nsp: minimum wait time must be 1 second")
+        wait_time = 3
+
+    print(f"\nsp: Raising GPIO [{pin_number}] for [{wait_time}] seconds")
+    # Raise GPIO TODO
+    time.sleep(wait_time)
+
+
+def shutdown(reason="Not specified"):
+    '''
+    Called by process control to shutdown the pi.
+
+    args: string, reason for shutting down
+    '''
+   
+    raise_GPIO(config_GPIO_pin_num ,config_GPIO_on_time)
+   
     print(f"sp: Shutting down due to: {reason}")
-    print("sp: Raising 'turn me off' GPIO pin...")
-    # TODO: Code to raise GPIO pin goes here
-    print("sp: GPIO pin raised. Shutting down now...")
-    # TODO: Code to shutdown the system goes here
-
-    print("sp: Shutting down Raspberry Pi...")
-    print("sp: THIS SCRIPT IS NOT FINISHED OR TESTED!!!")
+   
+    # call("sudo shutdown -h now", shell=True)

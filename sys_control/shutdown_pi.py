e7597ba92991b70ce34c3b970ff34b7da0d4cc57
Aksel-XD <86744896+Aksel-XD@users.noreply.github.com>
2025-11-20 13:09:51 -1000

    Added mission mode to config and shutdown.

--- sys_control/shutdown_pi.py
+++ sys_control/shutdown_pi.py
@@ -9,7 +9,16 @@ ID:     sp
 
 from subprocess import call
 import time
+import sys 
+import os 
+import yaml
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
 
+###################################################################
+# CONFIGURATION DEFAULTS
+config_file = project_root + '/config/config.yaml'
+###################################################################
 
 def raise_GPIO(pin_number, wait_time):
     # NOTE: No error checking on valid pin number.
@@ -31,8 +40,28 @@ def shutdown(reason="Not specified", config_GPIO_on_time=15, config_GPIO_pin_num
     args: string, reason for shutting down
     '''
    
+
     raise_GPIO(config_GPIO_pin_num ,config_GPIO_on_time)
    
     print(f"sp: Shutting down due to: {reason}")
-   
-    # call("sudo shutdown -h now", shell=True)
+    
+    # TODO: THIS HAS NOT BEEN TESTED!!!!
+    # Check if the system is in mission mode
+    try: 
+        with open(config_file, 'r') as file:
+            config = yaml.safe_load(file)
+    except Exception as e:
+        print(f"sp: Error reading config file: {e}")
+        print("sp: Unable to verify mission mode, proceeding with shutdown.")
+        # Shutdown in the event that we cannot read the config
+        call("sudo shutdown -h now", shell=True)
+        return
+    
+    # Prevent shutdown in the event of non-mission mode (aka you're bench top testing)
+    if not config.get('mission_mode', False):
+        print("sp: Mission mode not enabled, aborting shutdown.")
+        return
+    else: 
+        # Shutdown the Pi 
+        call("sudo shutdown -h now", shell=True)
+    
